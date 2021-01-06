d36c6475838767b273bc105779edea07cf78b81f
Vasanth Velayudham <vvk.victory@gmail.com>
2021-01-06 15:33:22 +0530

    updated spleeterTrainer file with proper comments

--- SpleeterTrainer.py
+++ SpleeterTrainer.py
@@ -1,3 +1,6 @@
-import SpleeterTrain
+import SpleeterTrainBase
 
-SpleeterTrain.trainModel(101, 50)
+noOfTrainingRuns = 101
+saveModelAtEveryRun = 50
+#models will be saved under the directory 'spleeter_saved_model_dir'
+SpleeterTrainBase.trainModel(noOfTrainingRuns, saveModelAtEveryRun)
